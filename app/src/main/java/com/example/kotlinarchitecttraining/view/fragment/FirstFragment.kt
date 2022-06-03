f838a21dc3cdbbc9fa9c12c29a8c8845677af627
kataoka-kk <90675281+kataoka-kk@users.noreply.github.com>
2022-06-03 12:11:42 +0900

    OS共有ロジック

--- app/src/main/java/com/example/kotlinarchitecttraining/view/fragment/FirstFragment.kt
+++ app/src/main/java/com/example/kotlinarchitecttraining/view/fragment/FirstFragment.kt
@@ -1,5 +1,6 @@
 package com.example.kotlinarchitecttraining.view.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -12,6 +13,7 @@ import com.example.kotlinarchitecttraining.R
 class FirstFragment : Fragment() {
 
     private lateinit var nextBtn: Button
+    private lateinit var shareBtn: Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,6 +22,7 @@ class FirstFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_first, container, false)
 
         nextBtn = view.findViewById(R.id.next_btn)
+        shareBtn = view.findViewById(R.id.image_share_btn)
 
         return view
     }
@@ -29,6 +32,18 @@ class FirstFragment : Fragment() {
 
         nextBtn.setOnClickListener {
             findNavController().navigate(R.id.first_to_second)
+        }
+
+        shareBtn.setOnClickListener {
+            // textの他アプリ共有の仕方
+            val sendIntent: Intent = Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_TEXT, "This is my text to send.")
+                type = "text/plain"
+            }
+
+            val shareIntent = Intent.createChooser(sendIntent, null)
+            startActivity(shareIntent)
         }
     }
 }
